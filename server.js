c316d53a79c45ac13b0d04b0bd74302b63c68869
Robin Johny K <robinjohnyk@gmail.com>
2026-03-19 20:33:24 +0530

    fix mongoose options

--- server.js
+++ server.js
@@ -7,10 +7,9 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection (we'll update later if needed)
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("MongoDB Error:", err));
 .then(() => console.log("MongoDB connected"))
 .catch(err => console.error("MongoDB Error:", err));
 
